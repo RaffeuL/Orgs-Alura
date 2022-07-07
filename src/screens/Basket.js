afc97cadb62e4e1a6e4fb45359170bcfa2a16a9c
RaffeuL <rafael91.lisboa@hotmail.com>
2022-07-07 17:20:56 -0300

    creates styles for basket component

--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image,Text, StyleSheet, Dimensions } from 'react-native';
+import topo from '../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Basket() {
-    return <Text>Basket</Text>
+    return <>
+    <Image source={topo}  style={styles.topo} />
+    <Text style={styles.title} >Basket details</Text>
+    </>
+    
 }
+
+const styles = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    title: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16,
+    }
+})
