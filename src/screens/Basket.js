2dc18c393edc3ca273ab2558a18d682a94909b4b
RaffeuL <rafael91.lisboa@hotmail.com>
2022-07-07 17:26:15 -0300

    refactor assests directory

--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Image,Text, StyleSheet, Dimensions } from 'react-native';
-import topo from '../assets/topo.png';
+import top from '../../assets/topo.png';
 
 const width = Dimensions.get('screen').width;
 
 export default function Basket() {
     return <>
-    <Image source={topo}  style={styles.topo} />
+    <Image source={top}  style={styles.top} />
     <Text style={styles.title} >Basket details</Text>
     </>
     
 }
 
 const styles = StyleSheet.create({
-    topo: {
+    top: {
         width: "100%",
         height: 578 / 768 * width,
     },
