b89b32a107796bf8c57e48982a7100e20ac8cbe8
RaffeuL <rafael91.lisboa@hotmail.com>
2022-07-11 15:14:44 -0300

    import fonts

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
 import Basket from './src/screens/Basket';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "MonteSerratRegular": Montserrat_400Regular,
+    "MonteSerratBold": Montserrat_700Bold,
+  })
+
+  if(!fontsLoaded){
+    return <View />
+  }
   return (
     <SafeAreaView>
       <StatusBar />
--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -8,20 +8,19 @@ const width = Dimensions.get('screen').width;
 
 export default function Basket() {
     return <>
-    <Image source={top}  style={styles.top} />
-    <Text style={styles.title} >Basket details</Text>
+        <Image source={top}  style={styles.top} />
+        <Text style={styles.title} >Basket details</Text>
 
-    <View style={styles.basket}>
-        <Text style={styles.name}>Vegetable Basket</Text>
-        <View style={styles.farm}>
-            <Image source={logo} style={styles.farmImage}/>
-            <Text style={styles.farmName}>Jenny Jack Farm</Text>
+        <View style={styles.basket}>
+            <Text style={styles.name}>Vegetable Basket</Text>
+            <View style={styles.farm}>
+                <Image source={logo} style={styles.farmImage}/>
+                <Text style={styles.farmName}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={styles.description}>A basket with carefully selected products from farm to your kitchen</Text>
+            <Text style={styles.price}>R$ 40,00 </Text>
         </View>
-        <Text style={styles.description}>A basket with carefully selected products from farm to your kitchen</Text>
-        <Text style={styles.price}>R$ 40,00 </Text>
-    </View>
-    </>
-    
+    </>  
 }
 
 const styles = StyleSheet.create({
@@ -47,7 +46,7 @@ const styles = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontWeight: "bold",
+        fontFamily: "MonteSerratBold",
 
     },
     farm: {
@@ -62,6 +61,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
+        fontFamily: "MonteSerratRegular",
 
     },
     description: {
